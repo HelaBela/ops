d1eddd1fe9f1e6dfc507ebeff7b075ba8bc19bc7
Helena Franczak <helena.franczak@myob.com>
2021-08-25 16:46:22 +1000

    added a healthcheck

--- main.go
+++ main.go
@@ -4,17 +4,38 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
-
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "home page endpoint")
-}
 
 func handleRequest() {
 	http.HandleFunc("/", homePage)
+	http.HandleFunc("/health", healthCheck)
 	log.Fatal(http.ListenAndServe(":8081", nil))
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("home page endpoint"))
+	//fmt.Fprintf(w, "home page endpoint")
+	//io.WriteString(w, "home page")
 }
 
 func main() {
 	handleRequest()
 }
+
+func healthCheck(w http.ResponseWriter, r *http.Request) {
+
+	w.WriteHeader(200)
+	fmt.Fprintf(w, "its ok")
+
+}
+
+//getting the pipeline to build this and package
+//github actions
+
+type Metadata struct {
+	commit      string
+	lastUpdated time.Time
+}
+
+//tracablity
